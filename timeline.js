eefa8042e64982066d768e6f1c53eb7efefc7453
Jian (Robin) ZHAO <jzhao@astreetpress.com>
2013-12-12 13:18:29 +0800

    support yy-mm-dd format date

--- timeline.js
+++ timeline.js
@@ -5,6 +5,21 @@ $.fn.timeline = function(json){
   $(this).empty();
   var that = this;
 
+  this.getPoints = function(date_string) {
+    var parts = date_string.split('-');
+    var points = 0;
+    points = 12 * parseInt(parts[0]) + parseInt(parts[1]);
+    console.log(parseInt(points));
+    return points;
+  }
+  this.getMonth = function(points,display_month) {
+    var rtn = parseInt(points / 12);
+    if ( display_month) {
+     rtn += '-' + parseInt(points % 12); 
+    }
+    return rtn;
+  }
+
   // Set width.
   $(this).css({width: json.width + 'px'});
   // Add skeleton divs.
@@ -15,10 +30,15 @@ $.fn.timeline = function(json){
   $(this).append(tl_timescale);
   $(this).append(tl_slider);
 
+  var min_point = this.getPoints(json.min_date); 
+  var max_point = this.getPoints(json.max_date);
+
   // Calculate scale ratio.
-  var additional = 20;
-  var diff = json.max_date - json.min_date + additional * 2; 
-  var ratio = json.ratio ? json.ratio : 50;
+  var additional = 50; //  months before and after timeline.
+  var diff = max_point - min_point + additional * 2; 
+  console.info(diff);
+  
+  var ratio = json.ratio ? json.ratio : 10;
   tl_timescale.css({width: ( diff * ratio )  + 'px'});
 
   // Slide the timescale.
@@ -29,22 +49,24 @@ $.fn.timeline = function(json){
       left: moved 
     });
   }
+  // Load content to top area.
   this.loadContent = function(){
 
   }
+  // Load timepoints to scale.
   this.loadTimescale = function(){
 
   }
 
   // Draw scale lines.
   var current_scale = null;
-  var scale_year = 0;
+  var scale_point = 0;
   for(var i=0; i<diff; i++){
-    scale_year = json.min_date - additional + i;
+    scale_point = min_point - additional + i;
     current_scale = $('<div class="scale"></div>');
-    current_scale.append('<span>' + scale_year + '</span>');
+    current_scale.append('<span>' + this.getMonth(scale_point,false) + '</span>');
     current_scale.css({left: i*ratio +'px'});
-    if( scale_year % 10 === 0){
+    if( scale_point % 12 === 0){
       current_scale.addClass('scale-decade');
     }
     tl_timescale.append(current_scale);
@@ -53,7 +75,7 @@ $.fn.timeline = function(json){
   // Insert available date points. 
   $.each(json.points, function(i,n){
     var tl_body_container = $('<div class="tl-body-container"></div>');
-    var left = ( n.date - json.min_date + additional) * ratio; 
+    var left = ( that.getPoints(n.date) - min_point + additional) * ratio; 
 
     if ( i === json.focus ) {
       tl_timescale.css({left: (-left+$(that).width()/2) + 'px'});
